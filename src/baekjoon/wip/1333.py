454e854a177f265a3d793a32db37ccd5c8142d1d
HK Yooun <yooun.hk@gmail.com>
2021-11-27 12:01:57 +0900

    Remove unsolved

--- src/baekjoon/wip/1333.py
+++
@@ -1,5 +0,0 @@
-input = [int(i) for i in input().split()]
-n, l, d = input
-
-for i in range(n):
-    track = [i for i in range(l + 6)]
--- src/baekjoon/wip/1942.py
+++
@@ -1,3 +0,0 @@
-for _ in range(3):
-    start, end = input().split()
-   
